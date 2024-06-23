fdf44d24021669de324166addff8f26d51b1f800
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-06-23 23:21:01 +0500

    masterclass

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-salom
+function arr(style) {
+    if (style === 'tugma1') {
+        document.body.style.backgroundColor = "yellow";
+        document.querySelector('.image-block').style.backgroundColor = "green";
+        document.querySelector('.text-block').style.backgroundColor = "yellow";
+        document.querySelector('.card-block').style.backgroundColor = "white";
+    } else if (style === 'tugma2') {
+        document.body.style.backgroundColor = "red";
+        document.querySelector('.image-block').style.backgroundColor = "#ffe0e0";
+        document.querySelector('.text-block').style.backgroundColor = "#ffcccc";
+        document.querySelector('.card-block').style.backgroundColor = "#ffb3b3";
+    } else if (style === 'tugma3') {
+        document.body.style.backgroundColor = "green";
+        document.querySelector('.image-block').style.backgroundColor = "#e0ffe0";
+        document.querySelector('.text-block').style.backgroundColor = "#ccffcc";
+        document.querySelector('.card-block').style.backgroundColor = "#b3ffb3";
+    }
+}
